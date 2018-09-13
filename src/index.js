693dc03db1b501f6528b4ae8264638b9ef5d233f
Siarhei Kvachonak <segrey1989@tut.by>
2018-09-13 22:29:03 +0300

    Money-Exchange: full implementation.All tests done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+if(currency <= 0)
+return {};
+
+if(currency > 10000){
+  var obj = {};
+  obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+  return obj;
+}
+
+var myMap = {};
+currency = count_money(50, myMap, currency);
+currency =  count_money(25, myMap, currency);
+currency = count_money(10, myMap, currency);
+currency = count_money(5, myMap, currency);
+currency = count_money(1, myMap,currency);
+return myMap;
+}
+
+function count_money(num, obj, currency){
+    var count = 0;
+    while(currency >= num){
+      currency -= num;
+      count += 1;
+  }
+  if(count > 0)
+  switch(num){
+      case 50:
+       obj.H = count; break;
+      case 25:
+       obj.Q = count; break;
+      case 10:
+      obj.D = count; break;
+      case 5:
+      obj.N = count; break;
+      case 1:
+       obj.P = count; break;
+  }
+  return currency;
 }
